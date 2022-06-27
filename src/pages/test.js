c780a9d3c6547f00c5c0724697ab32bd76ea07c3
jwp345 <jwp135@naver.com>
2022-06-27 17:09:27 +0900

    테스트페이지 추가

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const TestPage = () => {
+  return (
+    <main>
+      <title>테스트 스페이스</title>
+      <h1>테스트 스페이스 입니다.</h1>
+    </main>
+  );
+};
+
+export default TestPage;
